f1452f4d691293cc089b0efa1e562a6d2c67d375
Jonathan Stewart <jonjonstew09@gmail.com>
2026-06-17 19:32:57 -0400

    Add files via upload

---
+++ character_creator.py
@@ -0,0 +1,62 @@
+print("Welcome to the Character Creator!")
+print("You can choose one of the three classes: "
+      "\nWarrior"
+      "\nMage"
+      "\nArcher")
+character = input("\nChoose a class: ")
+
+if character == "Warrior":
+    print("\nWarrior")
+    print("Description: Strong frontline fighter who can take lots of damage.")
+    print("\nStats")
+    print(" Strength: 10"
+          "\n Defense: 9"
+          "\n Speed: 4"
+          "\n Magic: 1"
+          "\n Health: 100")
+    print("\nAbilities")
+    print(" Power Strike – Deals extra damage."
+          "\n Shield Block – Reduces incoming damage."
+          "\n Battle Cry – Increases strength for a short time.")
+elif character == "Mage":
+    print("\nMage")
+    print("Description: Uses powerful spells but has low health.")
+    print("\nStats")
+    print(" Strength: 2"
+          "\n Defense: 3"
+          "\n Speed: 5"
+          "\n Magic: 10"
+          "\n Health: 60")
+    print("\nAbilities")
+    print(" Fireball – High magic damage."
+          "\n Ice Blast – Slows enemies."
+          "\n Heal - Restores health.")
+elif character == "Archer":
+    print("\nArcher")
+    print("Description: Fast ranged fighter with balanced stats.")
+    print("\nStats")
+    print(" Strength: 6"
+          "\n Defense: 4"
+          "\n Speed: 10"
+          "\n Magic: 2"
+          "\n Health: 80")
+    print("\nAbilities")
+    print(" Rapid Shot - Fires multiple arrows."
+          "\n Piercing Arrow - Ignores some defense."
+          "\n Dodge Roll - Increases chance to avoid attacks.")
+
+
+##Archer
+##
+##Description: Fast ranged fighter with balanced stats.
+##
+##Stats
+##Strength: 6
+##Defense: 4
+##Speed: 10
+##Magic: 2
+##Health: 80
+##Abilities
+##Rapid Shot – Fires multiple arrows.
+##Piercing Arrow – Ignores some defense.
+##Dodge Roll – Increases chance to avoid attacks.
